43c3af3ef5fb1194eace0fe742139fd3e2d58f00
02teedd <magwanyathabang0@gmail.com>
2024-01-21 20:54:30 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include  <stdio.h>
+
+/**
+ * main - function that prints a string followed by new line.
+ *
+ * return: 0 if successful.
+ */
+int main(void)
+
+{
+	char c;
+
+	printf("Puts with recursion");
+		scanf("%c", c);
+		return (0);
+}
