bba20f6f4ec32d42efeed2959395b863d204c18d
02teedd <magwanyathabang0@gmail.com>
2024-01-25 00:11:58 -0800

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (newDog == NULL)
+	{
+		return (NULL);
+	}
+
+	newDog->name = strdup(name);
+	newDog->owner = strdup(owner);
+
+	if (newDog->name == NULL || newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog->owner);
+		free(newDog);
+		return (NULL);
+	}
+
+	newDog->age = age;
+
+	return (newDog);
+}
