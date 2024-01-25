19c482a8319ca89ed3598b5b353d7a14e44798ff
02teedd <magwanyathabang0@gmail.com>
2024-01-25 01:16:52 -0800

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
+
+void free_dog(dog_t *dog) {
+    if (dog != NULL) {
+        free(dog->name);
+        free(dog->owner);
+        free(dog);
+    }
+}
+
+int main() {
+    dog_t *myDog = /* Assume you have a valid dog created */;
+
+    free_dog(myDog);
+
+    return 0;
+}
