c85f6806e38b34dbacddbcfcc798470642295509
02teedd <magwanyathabang0@gmail.com>
2024-02-02 03:46:27 -0800

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
