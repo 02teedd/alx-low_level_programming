6d91024f973651716259671a005779e75081f0e7
02teedd <magwanyathabang0@gmail.com>
2024-02-02 07:07:25 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-__attribute__((constructor)) void print_before_main()
+__attribute__((constructor)) void print_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
