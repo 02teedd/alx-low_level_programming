b7fe04b87268f097e2b2e0c7c0bb4c395623bc10
02teedd <magwanyathabang0@gmail.com>
2024-02-03 10:57:48 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,44 +2,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *originalDest = dest;
+	char *dest_end = dest;
 
-	while (*dest != '\0'}
+	while (*dest_end != '\0')
 	{
-		dest++;
+		dest_end++;
 	}
 
-	while (^src != '\0')
+	while (*src != '\0')
 	{
-	*dest = *src:
-	dest++;
-	src++;
+		*dest_end++ = *src++;
 	}
 
-	*dest = '\0';
+	*dest_end = '\0';
 
-	return originalDest;
-}
-
-int main()
-{
-    char str1[50] = "Hello";
-    charstr2[] = "World";
-
-    printf("%s\n", str1);
-    printf("%s\n", str2);
-
-    _strcat(str1, " ");
-    _strcat(cat1, str2);
-
-    printf("%s\n", str1);
-    printf("%s\n", str2);
-
-    _strcat(str2, " ")
-    _strcat(str2, "Hello World!");
-
-    printf("%s\n", str1);
-    printf("%s\n", str2);
-
-    return (0);
+	return (dest);
 }
