74d60d37705a25039039c00ad6a7af56fa9cb513
02teedd <magwanyathabang0@gmail.com>
2024-02-08 06:51:30 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * _print_rev_recursion - prints a string in revrse using recursion.
+ * @s: The string to be printed in revrse.
+ *
+ * Description:
+ * This function recursively prints a given string in reverse order.
+ * the base case is when the string pointer reachs the null terminator.
+ * otherwise, the function prints the last character, then recursively calls itself with a pointer to a substring excluding the last character.
+ */
 void _print_rev_recursion(const char *str)
 
 {
