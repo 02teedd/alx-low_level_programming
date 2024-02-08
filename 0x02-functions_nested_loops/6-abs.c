da5e22e5dde8a71d7ab5ed694a8b63c2908a7f12
02teedd <magwanyathabang0@gmail.com>
2024-02-08 14:10:29 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,7 @@
 #include "main.h"
+#include <stdio.h>
 
-int absolute_value(int n)
+Int _abs(int n)
 {
-	if (n < 0)
-	{
-		return (-0);
-	}
-	else
-	{
-		return (n);
-	}
+	return (n < 0) ? -n : n;
 }
