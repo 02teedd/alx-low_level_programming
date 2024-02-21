bbd260ad7fedcea021d38ec64f1d0d19fcc9a85e
02teedd <magwanyathabang0@gmail.com>
2024-02-20 16:36:56 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,57 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
+#include "stddef.h"
+#include "stdlib.h"
+
+/**
+ * str_concat - concatinates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to newly allocated space
+ */
 
 char *str_concat(char *s1, char *s2)
 {
-	int len1 = 0, len2 = 0;
-	char *result;
+	int s1len = 0;
+	int s2len = 0;
+	int i;
+	char *output;
 
-	if (s1)
-	{
-		while (s1[len1])
-		{
-			len1++;
-		}
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+		s1len++;
+	for (i = 0; s2[i] != '\0'; i++)
+		s2len++;
 
-	if (s2)
-	{
-		while (s2[len2])
-		{
-			len2++;
-		}
-	}
+	output = malloc(sizeof(char) * (s1len + s2len) + 1);
 
-	result = malloc(len1 + len2 + 1);
-
-	if (!result)
-	{
-		return NULL;
-	}
-
-	if (s1)
-	{
-		for (int i = 0; i < len2; i++)
-			{
-				/* ... loop body ... */
-			}
-		{
-			result[i] = s1[i];
-		}
-	}
-
-	if (s2)
-	{
-		for (int i = 0; i < len2; i++)
-		{
-			/** ... loop body ... */
-		}
-		{
-			result[len1 + i] = s2[i];
-		}
-	}
-
-	result[len1 = len2] = '\0';
-
-	return result;
+	if (output == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		output[i] = s1[i];
+	for (i = 0; s2[i] != '\0'; i++)
+		output[s1len + i] = s2[i];
+	return (output);
 }
