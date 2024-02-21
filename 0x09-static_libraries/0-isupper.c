c24c7a378b8927fca2c9939aa79912a524c93d9a
02teedd <magwanyathabang0@gmail.com>
2024-02-21 15:23:42 -0800

    libmy.a

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- * _isupper - uppercase letters
- * @c: char to check
- *
- * Return: 0 or 1
+ * _isupper - checks for uppercase character
+ * @c: function parameter
+ * Return: 1 on success 0 on failure
  */
-
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
