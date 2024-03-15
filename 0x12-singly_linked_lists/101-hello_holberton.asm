556d7e7ffc5be839cb8c18647af2afc18cfc31be
02teedd <magwanyathabng0@gmail>
2024-03-15 09:45:23 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,9 @@
-section .data
-    message db "Hello, Holberton\n", 0 ; String with newline
-
-section .text
-    global _start
-
-_start:
-    ; Set up arguments for printf
-    mov rdi, message ; First argument: string addres
-    mov rax, 0 ; No floating- point arguments
-    call printf ; Call printf function
-
-    ; Exit gracefully
-    mov rax, 60 ; system call number for exit
-    mov rdi, 0 ; Exit code 0
-    syscall
+global  main
+        extern printf
+main:
+        mov edi, format
+        xor eax, eax
+        call printf
+        mov     eax, 0
+        ret
+format: db 'Hello, Holberton', 0
